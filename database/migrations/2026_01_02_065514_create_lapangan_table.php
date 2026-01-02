0c40c4e8fcb36c939cda57d6b3b8b2d73e00649a
abu <abubalawi770@gmail.com>
2026-01-02 14:21:28 +0700

    Initial commit

---
+++ database/migrations/2026_01_02_065514_create_lapangan_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lapangan', function (Blueprint $table) {
+            $table->id('id_lapangan'); // Primary Key custom name
+            $table->string('nama_lapangan', 50);
+            $table->enum('kategori', ['VIP', 'Reguler']);
+            $table->integer('harga_per_jam');
+            $table->enum('status_lapangan', ['tersedia', 'perawatan']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lapangan');
+    }
+};
---
+++ database/migrations/2026_01_02_065903_create_reservasi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservasi', function (Blueprint $table) {
+            $table->id('id_reservasi'); // Primary Key custom name
+
+            // Foreign Keys
+            $table->foreignId('id_pelanggan')->constrained('pelanggan', 'id_pelanggan');
+            $table->foreignId('id_lapangan')->constrained('lapangan', 'id_lapangan');
+
+            $table->date('tanggal');
+            $table->time('jam_mulai');
+            $table->time('jam_selesai');
+            $table->integer('total_bayar');
+            $table->enum('status_reservasi', ['booked', 'selesai', 'batal']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservasi');
+    }
+};
